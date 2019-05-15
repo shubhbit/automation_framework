378aa3184d1ac4196be67e269942d6f396bec821
Shubham <shubham.jagdeesh@gmail.com>
2019-05-15 10:52:11 +0530

    1st commit

---
+++ src/features/employee.feature
@@ -0,0 +1,27 @@
+@tags @tag
+Feature: Employee CRUD
+  this is a feature file of an employee record life-cycle in an organization
+
+  Background: some requirement of this test
+    Given some setup condition
+      And some other setup action
+
+  Scenario: Create an record for new employee
+      Given some condition
+       When some action is taken
+       Then some result is expected.
+
+  Scenario: Update an existing employee record
+      Given some other condition
+       When some action is taken
+       Then some other result is expected.
+
+  Scenario: Get an existing employee record
+      Given some other condition
+       When some action is taken
+       Then some other result is expected.
+
+  Scenario: Delete an existing employee record
+      Given some other condition
+       When some action is taken
+       Then some other result is expected.
---
+++ src/steps/Employee_APIs/employees.py
@@ -0,0 +1,45 @@
+from requests import request
+
+
+class Employee(object):
+    """
+    Wrapper class to provide functionalities over Employee CRUD operations.
+    """
+    def __init__(self):
+        pass
+
+    def register_employee(self, data):
+        """
+        method to create a new entry for an employee
+        :return:
+        """
+        response = request("POST", create_url, json=data)
+        assert response.status_code == 200
+        return response.json()
+
+    def update_employee(self, data):
+        """
+        method to edit existing employee record
+        :return:
+        """
+        response = request("PUT", update_url, json=data)
+        assert response.status_code == 200
+        return response.json()
+
+    def get_employee(self, id):
+        """
+        method to get an employee record
+        :return:
+        """
+        response = request("GET", get_url)
+        assert response.status_code == 200
+        return response.json()
+
+    def delete_employee(self):
+        """
+        method to delete an existing record
+        :return:
+        """
+        response = request("DELETE", delete_url)
+        assert response.status_code == 200
+        return response.json()
